efbff9aea534ad0489bea66467b920531dd8d3dd
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2019-07-12 14:12:05 +0200

    Fix comment in push_to_pypi.sh script

--- push_to_pypi.sh
+++ push_to_pypi.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Remember to `git checkout <tag>` to the correct branch before running this script!
+# Remember to `git checkout <tag>` to the correct version tag before running this script!
 python3 setup.py sdist \
 && pip3 install --upgrade twine \
 && twine check dist/* \
