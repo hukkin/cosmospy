ec41daa6f44092e88f7be96afbe9b7b2bb53ac70
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2019-11-03 02:37:03 +0100

    Order public methods first

--- cosmospy/transactions.py
+++ cosmospy/transactions.py
@@ -32,37 +32,15 @@ class Transaction:
         self.msgs: List[dict] = []
 
     def add_atom_transfer(self, recipient: str, amount: int) -> None:
-        self.msgs.append(
-            {
-                "type": "cosmos-sdk/MsgSend",
-                "value": {
-                    "from_address": privkey_to_address(self.privkey),
-                    "to_address": recipient,
-                    "amount": [{"denom": "uatom", "amount": str(amount)}],
-                },
-            }
-        )
-
-    def _get_sign_message(self) -> Dict[str, Any]:
-        return {
-            "chain_id": self.chain_id,
-            "account_number": str(self.account_num),
-            "fee": {"gas": str(self.gas), "amount": [{"amount": str(self.fee), "denom": "uatom"}]},
-            "memo": self.memo,
-            "sequence": str(self.sequence),
-            "msgs": self.msgs,
+        transfer = {
+            "type": "cosmos-sdk/MsgSend",
+            "value": {
+                "from_address": privkey_to_address(self.privkey),
+                "to_address": recipient,
+                "amount": [{"denom": "uatom", "amount": str(amount)}],
+            },
         }
-
-    def _sign(self) -> str:
-        message_str = json.dumps(self._get_sign_message(), separators=(",", ":"), sort_keys=True)
-        message_bytes = message_str.encode("utf-8")
-
-        privkey = PrivateKey(bytes.fromhex(self.privkey))
-        signature = privkey.ecdsa_sign(message_bytes)
-        signature_compact = privkey.ecdsa_serialize_compact(signature)
-
-        signature_base64_str = base64.b64encode(signature_compact).decode("utf-8")
-        return signature_base64_str
+        self.msgs.append(transfer)
 
     def get_pushable_tx(self) -> str:
         pubkey = privkey_to_pubkey(self.privkey)
@@ -87,3 +65,24 @@ class Transaction:
             "mode": self.sync_mode,
         }
         return json.dumps(pushable_tx, separators=(",", ":"))
+
+    def _sign(self) -> str:
+        message_str = json.dumps(self._get_sign_message(), separators=(",", ":"), sort_keys=True)
+        message_bytes = message_str.encode("utf-8")
+
+        privkey = PrivateKey(bytes.fromhex(self.privkey))
+        signature = privkey.ecdsa_sign(message_bytes)
+        signature_compact = privkey.ecdsa_serialize_compact(signature)
+
+        signature_base64_str = base64.b64encode(signature_compact).decode("utf-8")
+        return signature_base64_str
+
+    def _get_sign_message(self) -> Dict[str, Any]:
+        return {
+            "chain_id": self.chain_id,
+            "account_number": str(self.account_num),
+            "fee": {"gas": str(self.gas), "amount": [{"amount": str(self.fee), "denom": "uatom"}]},
+            "memo": self.memo,
+            "sequence": str(self.sequence),
+            "msgs": self.msgs,
+        }
