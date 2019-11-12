30baf3c84d3b05535cdf0026316513c1aac536b0
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2019-11-12 02:09:01 +0100

    Add a comment for SyncMode

--- cosmospy/typing.py
+++ cosmospy/typing.py
@@ -5,7 +5,8 @@ if sys.version_info < (3, 8):
 else:
     from typing import Literal, TypedDict
 
-
+# Valid transaction broadcast modes for the `POST /txs` endpoint of the
+# Cosmos REST API.
 SyncMode = Literal["sync", "async", "block"]
 
 
