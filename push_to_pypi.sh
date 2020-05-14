114a70539c283def82078591cc8795bc2c9e86af
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-05-15 01:36:09 +0200

    Use poetry (#6)

--- push_to_pypi.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Remember to `git checkout <tag>` to the correct version tag before running this script!
-python3 setup.py sdist \
-&& pip3 install --upgrade twine \
-&& twine check dist/* \
-&& twine upload dist/*
