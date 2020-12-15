2739b3bdfbd9b49ff32192cce905ad7e52801f82
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-12-15 03:41:34 +0200

    Move to github actions

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-[![Build Status](https://travis-ci.com/hukkinj1/cosmospy.svg?branch=master)](https://travis-ci.com/hukkinj1/cosmospy)
+[![Build Status](https://github.com/hukkinj1/cosmospy/workflows/Tests/badge.svg?branch=master)](https://github.com/hukkinj1/cosmospy/actions?query=workflow%3ATests+branch%3Amaster+event%3Apush)
 [![codecov.io](https://codecov.io/gh/hukkinj1/cosmospy/branch/master/graph/badge.svg)](https://codecov.io/gh/hukkinj1/cosmospy)
 [![PyPI version](https://img.shields.io/pypi/v/cosmospy)](https://pypi.org/project/cosmospy)
-[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 
 # cosmospy
 
--- cosmospy/__init__.py
+++ cosmospy/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "5.1.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+
 from hdwallets import BIP32DerivationError as BIP32DerivationError  # noqa: F401
 
 from cosmospy._transaction import Transaction as Transaction  # noqa: F401
@@ -6,5 +8,3 @@ from cosmospy._wallet import privkey_to_address as privkey_to_address  # noqa: F
 from cosmospy._wallet import privkey_to_pubkey as privkey_to_pubkey  # noqa: F401
 from cosmospy._wallet import pubkey_to_address as pubkey_to_address  # noqa: F401
 from cosmospy._wallet import seed_to_privkey as seed_to_privkey  # noqa: F401
-
-__version__ = "5.1.1"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
